6f0ab6db816aa0e2a2160ce6b89d9078283b03ba
Benjamin Jurkowski <benjamin.jurkowski@gmail.com>
2023-01-04 15:08:21 +0100

    Update pkmn suggestions

---
+++ _app/immutable/chunks/stores-d0087e11.js
@@ -0,0 +1,1 @@
+import"./index-61c90870.js";import{s as e}from"./singletons-1120b2fa.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
