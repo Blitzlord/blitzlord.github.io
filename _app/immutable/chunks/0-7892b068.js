78924cd3f65e1f8ba851e34813229714391c97e4
Benjamin Jurkowski <benjamin.jurkowski@gmail.com>
2023-01-08 19:13:44 +0100

    update

---
+++ _app/immutable/chunks/0-7892b068.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-39016b64.js";export{t as component};
---
+++ _app/immutable/chunks/1-52b3a2d9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-298b859f.js";export{t as component};
