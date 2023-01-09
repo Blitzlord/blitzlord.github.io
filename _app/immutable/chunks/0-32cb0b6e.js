98aede8b2a37d8e2baa9834775b3e6ce38a2a395
Benjamin Jurkowski <benjamin.jurkowski@gmail.com>
2023-01-09 17:22:40 +0100

    fix pokemon suggestions when changing tera type

---
+++ _app/immutable/chunks/0-32cb0b6e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-a4ec7c68.js";export{t as component};
---
+++ _app/immutable/chunks/2-2fc915f4.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-90a7f2f4.js";import{default as t}from"../components/pages/_page.svelte-d2508be1.js";export{t as component,r as universal};
