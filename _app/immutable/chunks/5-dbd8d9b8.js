d9e161d54cec2126968f18112624b673ff8eb5bf
Benjamin Jurkowski <benjamin.jurkowski@gmail.com>
2023-03-20 18:03:17 +0100

    blissey

--- _app/immutable/chunks/5-dbd8d9b8.js
+++ _app/immutable/chunks/5-dbd8d9b8.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-e94d8ac7.js";import{default as t}from"../components/pages/type-table.html/_page.svelte-155300dd.js";export{t as component,r as universal};
+import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/type-table.html/_page.svelte-155300dd.js";export{t as component,r as universal};
