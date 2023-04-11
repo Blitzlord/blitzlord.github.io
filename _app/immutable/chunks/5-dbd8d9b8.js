c15a66b145363ff6c6f9d3df4f5a3c043da8f009
Benjamin Jurkowski <benjamin.jurkowski@gmail.com>
2023-04-11 17:18:14 +0200

    typho_beta

--- _app/immutable/chunks/5-dbd8d9b8.js
+++ _app/immutable/chunks/5-dbd8d9b8.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/type-table.html/_page.svelte-155300dd.js";export{t as component,r as universal};
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/type-table.html/_page.svelte-155300dd.js";export{t as component,r as universal};
